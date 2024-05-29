2fe31beeaa18e16d7612d796a67275cd5babc994
Inayat <inyat.nathani@gmail.com>
2024-05-29 17:58:19 +0200

    Initial commit

---
+++ src/main/java/se/inyat/myenterpriseapp/MyEnterpriseAppApplication.java
@@ -0,0 +1,14 @@
+package se.inyat.myenterpriseapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyEnterpriseAppApplication {
+
+    public static void main(String[] args) {
+
+        SpringApplication.run(MyEnterpriseAppApplication.class, args);
+    }
+
+}
---
+++ src/test/java/se/inyat/myenterpriseapp/MyEnterpriseAppApplicationTests.java
@@ -0,0 +1,13 @@
+package se.inyat.myenterpriseapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyEnterpriseAppApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
