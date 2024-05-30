f823cce9e477a05d89621e8717abc77f0a8e864a
inyat <inyat.nathani@gmail.com>
2024-05-30 16:32:36 +0200

    1.0

--- src/main/java/se/inyat/myenterpriseapp/MyEnterpriseApplication.java
+++ src/main/java/se/inyat/myenterpriseapp/MyEnterpriseApplication.java
@@ -7,6 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class MyEnterpriseApplication {
 
     public static void main(String[] args) {
+
         SpringApplication.run(MyEnterpriseApplication.class, args);
     }
 }
